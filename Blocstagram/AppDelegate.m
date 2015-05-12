0fa3d7b9af628b19cc6c605c19c1c359714af001
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-11 20:40:43 -0700

    exercise-26

--- Blocstagram/AppDelegate.m
+++ Blocstagram/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "ImagesTableTableViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -16,10 +18,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    return YES;
+    {
+        self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+        self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableTableViewController alloc] init]];
+        self.window.rootViewController = [[ImagesTableTableViewController alloc] init];
+        // Override point for customization after application launch.
+        self.window.backgroundColor = [UIColor whiteColor];
+        [self.window makeKeyAndVisible];
+        return YES;
 }
-
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
