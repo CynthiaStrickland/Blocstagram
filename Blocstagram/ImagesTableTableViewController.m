a95499cd7b17f1e416aadc5db5f796b2d4ba84e9
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-18 19:41:22 -0700

    infinite-scroll-pull2

--- Blocstagram/ImagesTableTableViewController.m
+++ Blocstagram/ImagesTableTableViewController.m
@@ -19,6 +19,11 @@
 
 @implementation ImagesTableTableViewController
 
+//- (void) dealloc
+//   {
+//       [[DataSource sharedInstance] removeObserver:self forKeyPath:@"mediaItems"];
+//   }
+
 - (id)initWithStyle:(UITableViewStyle)style
 {
     self = [super initWithStyle:style];
@@ -32,60 +37,83 @@
 }
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
+    
     [[DataSource sharedInstance] addObserver:self forKeyPath:@"mediaItems" options:0 context:nil];
+    
+        // *********** ADDING PULL TO REFRESH ************
+
+
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(refreshControlDidFire:) forControlEvents:UIControlEventValueChanged];
+    
     [self.tableView registerClass:[MediaTableViewCell class] forCellReuseIdentifier:@"mediaCell"];
 }
 
-//- (void) dealloc
-//   {
-//       [[DataSource sharedInstance] removeObserver:self forKeyPath:@"mediaItems"];
-//   }
+- (void) refreshControlDidFire:(UIRefreshControl *) sender {
+    [[DataSource sharedInstance] requestNewItemsWithCompletionHandler:^(NSError *error) {
+        [sender endRefreshing];
+    }];
+}
 
-- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if (object == [DataSource sharedInstance] && [keyPath isEqualToString:@"mediaItems"]) {
-        // We know mediaItems changes.  Let's see what kind of change it is.
-        NSKeyValueChange kindOfChange = [change[NSKeyValueChangeKindKey] unsignedIntegerValue];
-        
-        if (kindOfChange == NSKeyValueChangeSetting) {
-            //Someone set a brand new images array
-            [self.tableView reloadData];
-            
-        } else if (kindOfChange == NSKeyValueChangeInsertion ||
-                   kindOfChange == NSKeyValueChangeRemoval ||
-                   kindOfChange == NSKeyValueChangeReplacement) {
-            // We have an incremental change: inserted, deleted, or replaced images
+- (void) infiniteScrollIfNecessary {
+    // #3
+   NSIndexPath *bottomIndexPath = [[self.tableView indexPathsForVisibleRows] lastObject];
 
-            // Get a list of the index (or indices) that changed
-            NSIndexSet *indexSetOfChanges = change[NSKeyValueChangeIndexesKey];
-
-            // #1 - Convert this NSIndexSet to an NSArray of NSIndexPaths (which is what the table view animation methods require)
-            NSMutableArray *indexPathsThatChanged = [NSMutableArray array];
-            [indexSetOfChanges enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
-                NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:idx inSection:0];
-                [indexPathsThatChanged addObject:newIndexPath];
-            }];
-
-            // #2 - Call `beginUpdates` to tell the table view we're about to make changes
-            [self.tableView beginUpdates];
-
-            // Tell the table view what the changes are
-            if (kindOfChange == NSKeyValueChangeInsertion) {
-                [self.tableView insertRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
-            } else if (kindOfChange == NSKeyValueChangeRemoval) {
-                [self.tableView deleteRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
-            } else if (kindOfChange == NSKeyValueChangeReplacement) {
-                [self.tableView reloadRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
-            }
-            
-            // Tell the table view that we're done telling it about changes, and to complete the animation
-            [self.tableView endUpdates];
-        }
+    if (bottomIndexPath && bottomIndexPath.row == [DataSource sharedInstance].mediaItems.count - 1) {
+       // The very last cell is on screen
+        [[DataSource sharedInstance] requestOldItemsWithCompletionHandler:nil];
     }
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    [self infiniteScrollIfNecessary];
 }
 
 
 
+- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+        if (object == [DataSource sharedInstance] && [keyPath isEqualToString:@"mediaItems"]) {
+            // We know mediaItems changed.  Let's see what kind of change it is.
+            int kindOfChange = [change[NSKeyValueChangeKindKey] intValue];
+            
+            if (kindOfChange == NSKeyValueChangeSetting) {
+                // Someone set a brand new images array
+                [self.tableView reloadData];
+            }
+            else if (kindOfChange == NSKeyValueChangeInsertion ||
+                     kindOfChange == NSKeyValueChangeRemoval ||
+                     kindOfChange == NSKeyValueChangeReplacement) {
+                // We have an incremental change: inserted, deleted, or replaced images
+                
+                // Get a list of the index (or indices) that changed
+                NSIndexSet *indexSetOfChanges = change[NSKeyValueChangeIndexesKey];
+                
+                // Convert this NSIndexSet to an NSArray of NSIndexPaths (which is what the table view animation methods require)
+                NSMutableArray *indexPathsThatChanged = [NSMutableArray array];
+                [indexSetOfChanges enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
+                    NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:idx inSection:0];
+                    [indexPathsThatChanged addObject:newIndexPath];
+                }];
+                
+                // Call `beginUpdates` to tell the table view we're about to make changes
+                [self.tableView beginUpdates];
+                
+                // Tell the table view what the changes are
+                if (kindOfChange == NSKeyValueChangeInsertion) {
+                    [self.tableView insertRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
+                } else if (kindOfChange == NSKeyValueChangeRemoval) {
+                    [self.tableView deleteRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
+                } else if (kindOfChange == NSKeyValueChangeReplacement) {
+                    [self.tableView reloadRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
+                }
+                
+                // Tell the table view that we're done telling it about changes, and to complete the animation
+                [self.tableView endUpdates];
+            }
+        }
+    }
 
 #pragma mark - Table view data source
 
