776285319086df056cea67bba1eac8ee41d3510f
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-06-17 21:41:52 -0700

    exercise-42-filters

---
+++ Blocstagram/Blocstagram/Blocstagram/Blocstagram/Blocstagram/Blocstagram/Blocstagram/Blocstagram/Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Cynthia Whitlatch on 5/11/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
