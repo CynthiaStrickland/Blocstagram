886b7a81b16324990b53c635b84e8b25eca81513
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-06-19 14:15:27 -0700

    blocwatch

---
+++ Blocstagram WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Blocstagram WatchKit Extension
+//
+//  Created by Cynthia Whitlatch on 6/19/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
