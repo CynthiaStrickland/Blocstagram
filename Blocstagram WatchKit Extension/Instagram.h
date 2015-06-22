d4928f1d36f552c1ec23120aada3211195f5c1d8
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-06-21 23:17:32 -0700

    blocwatch-0621

---
+++ Blocstagram WatchKit Extension/Instagram.h
@@ -0,0 +1,16 @@
+//
+//  Instagram.h
+//  Blocstagram
+//
+//  Created by Cynthia Whitlatch on 6/21/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface Instagram : NSObject
+
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *Outlet;
+
+@end
---
+++ Blocstagram WatchKit Extension/Instagram.m
@@ -0,0 +1,13 @@
+//
+//  Instagram.m
+//  Blocstagram
+//
+//  Created by Cynthia Whitlatch on 6/21/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import "Instagram.h"
+
+@implementation Instagram
+
+@end
